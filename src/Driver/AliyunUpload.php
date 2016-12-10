b6591765c1c9631457a714abc4b4f62a075fe8f7
xiaoteng <616896861@qq.com>
2016-12-10 21:52:31 +0800

    change aliyun package

--- src/Driver/AliyunUpload.php
+++ src/Driver/AliyunUpload.php
@@ -1,23 +1,33 @@
 <?php
 namespace Qsnh\think\Upload\Driver;
 
-use JohnLui\AliyunOSS\AliyunOSS;
-use Aliyun\OSS\Exceptions\OssException;
+use OSS\OssClient;
+use OSS\Core\OssException;
 
 class AliyunUpload extends UploadDriverInterface
 {
 
 	protected $app = null;
 
+	protected $bucket = '';
+
+	protected $remote_url = '';
+
 	public function __construct($config)
 	{
-		$this->app = AliyunOSS::boot(
-			$config['oss_server'],
+		$this->app = new OssClient(
 			$config['access_key_id'],
-			$config['access_key_secret']
+			$config['access_key_secret'],
+			$config['oss_server']
 		);
 
-		$this->app->setBucket($config['bucket']);
+		$this->bucket = $config['bucket'];
+
+		$this->remote_url = $config['remote_url'];
+
+		if (substr($this->remote_url, -1, 1) != '/') {
+			$this->remote_url .= '/';
+		}
 	}
 
 	public function upload(\SplFileInfo $file)
@@ -27,14 +37,16 @@ class AliyunUpload extends UploadDriverInterface
 		$filepath = $file->getPath() . DIRECTORY_SEPARATOR . $filename;
 
 		try{
-        	$result = $this->app->uploadFile($filename, $filepath);
+        	$result = $this->app->uploadFile($this->bucket, $filename, $filepath);
 	    } catch(OssException $e) {
 	        $this->setError($e->getMessage());
 
 	        return false;
 	    }
 
-		return $this->app->getUrl($filename, new \DateTime("+1 day"));
+	    @unlink($file->getPathname());
+
+		return $this->remote_url . $filename;
 	}
 
 }
